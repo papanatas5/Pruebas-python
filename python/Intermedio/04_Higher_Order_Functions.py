b7603b46131e4c1e29d3ddc015b72642084dc9fd
Pedro Niello <niellop123@gmail.com>
2023-02-15 14:48:02 -0300

    funciones de orden superior

--- python/Intermedio/04_Higher_Order_Functions.py
+++ python/Intermedio/04_Higher_Order_Functions.py
@@ -1,1 +1,22 @@
 ### Funciones de orden superior ###
+
+
+def sum_one(value):
+    return value + 1
+
+def sum_five(value):
+    return value + 5
+
+def sum_two_values_and_add_value(first_value, second_value, f_sum):
+    return f_sum(first_value + second_value)
+
+print(sum_two_values_and_add_value(5, 2, sum_one))
+print(sum_two_values_and_add_value(5, 2, sum_five))
+
+### Closures ###
+
+def sum_ten():
+    def add(value):
+        return value + 10
+    return add
+sum_ten()
