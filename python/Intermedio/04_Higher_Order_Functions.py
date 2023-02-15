f5c0c8a2cbb8a1e98bf0a66e4820ab624518b710
Pedro Niello <niellop123@gmail.com>
2023-02-15 15:21:43 -0300

    Funciones de orden superior

--- python/Intermedio/04_Higher_Order_Functions.py
+++ python/Intermedio/04_Higher_Order_Functions.py
@@ -15,8 +15,45 @@ print(sum_two_values_and_add_value(5, 2, sum_five))
 
 ### Closures ###
 
-def sum_ten():
+def sum_ten(original_value):
     def add(value):
-        return value + 10
+        return value + 10 + original_value
     return add
-sum_ten()
+
+add_closure = sum_ten(1)
+print(add_closure(5))
+
+print((sum_ten(5))(1))
+
+### Funciones de orden superior predeterminadas en el editor###
+### Built-in higher order functions ###
+
+numbers = [2,5,10,21, 3, 35, 89]
+
+#map
+def multiply_two(number):
+    return number * 2
+
+print(list(map(multiply_two, numbers)))
+print(list(map(lambda number: number * 2, numbers)))
+
+# filter
+
+def filter_greater_than_ten(number):
+    if number > 10:
+        return True
+    return False
+
+print(list(filter(filter_greater_than_ten,numbers)))
+print(list(filter(lambda number: number > 10,numbers)))
+
+# reduce
+import functools
+
+def sum_two_values(first_value, second_value):
+    print(first_value)
+    print(second_value)
+    return first_value + second_value 
+
+print(functools.reduce(sum_two_values, numbers))
+
